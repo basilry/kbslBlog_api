da22d7d1669146dee1415f9214af7f7de2374bf9
basilry <basilry@gmail.com>
2024-09-07 22:57:45 +0900

    [feat] eslint, prettier 설정

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
-import express from 'express';
+import express from "express"
+import cors from "cors"
+import morgan from "morgan"
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(
+    cors({
+        origin: process.env.GITHUB_IO_PAGE,
+        methods: "*",
+    }),
+)
+app.use(morgan("dev"))
+app.use(express.json())
 
-export default app;
+app.get("/", (req, res) => {
+    res.send("API 서버가 정상적으로 동작하고 있습니다.")
+})
+
+app.use((err: Error, _req: express.Request, res: express.Response) => {
+    console.error(err.stack)
+    res.status(500).send("서버에서 오류가 발생했습니다.")
+})
+
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
-import app from './app';
+import app from "./app"
+import dotenv from "dotenv"
 
-const PORT = process.env.PORT || 3000;
+dotenv.config()
+
+const PORT = process.env.PORT || 3004
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    console.log(`Server is running on port ${PORT}`)
+})
